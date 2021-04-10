a5587b986af8a805468a905bdfea1c8236c77823
Vishal Agarwal <agarwaldel20@gmail.com>
2021-04-10 23:37:44 +0530

    Completed basic backend
    Basic functioning is complete. Next stage is debugging

--- main.js
+++ main.js
@@ -56,16 +56,103 @@ start();
 document.body.addEventListener("keyup", function(e) {
 	switch(e.key) {
 		case "ArrowLeft":
-			
+				for(let i=0;i<4;i++) {
+					let count=0;
+					for(let j=0;j<4;j++) {
+						if(arr[i][j]) {
+							arr[i][count++]=arr[i][j];
+						}
+					}
+					while(count<4) {
+						arr[i][count++]=0;
+					}
+				}
+				for(let i=0;i<4;i++) {
+					for(let j=0;j<3;j++) {
+						if(arr[i][j]==arr[i][j+1]) {
+							arr[i][j]+=arr[i][j+1];
+							for(let k=j+1;k<3;k++) {
+								arr[i][k]=arr[i][k+1];
+							}
+							arr[i][3]=0;
+						}
+					}
+				}
 			rannum();
 			display();break;
 		case "ArrowRight":
+				for(let i=0;i<4;i++) {
+					let count=3;
+					for(let j=3;j>=0;j--) {
+						if(arr[i][j]) {
+							arr[i][count--]=arr[i][j];
+						}
+					}
+					while(count>=0) {
+						arr[i][count--]=0;
+					}
+				}
+				for(let i=0;i<4;i++) {
+					for(let j=3;j>0;j--) {
+						if(arr[i][j]==arr[i][j-1]) {
+							arr[i][j]+=arr[i][j-1];
+							for(let k=j-1;k>0;k--) {
+								arr[i][k]=arr[i][k-1];
+							}
+							arr[i][0]=0;
+						}
+					}
+				}
 			rannum();
 			display();break;
 		case "ArrowUp":
+				for(let i=0;i<4;i++) {
+					let count=0;
+					for(let j=0;j<4;j++) {
+						if(arr[j][i]) {
+							arr[count++][i]=arr[j][i];
+						}
+					}
+					while(count<4) {
+						arr[count++][i]=0;
+					}
+				}
+				for(let i=0;i<4;i++) {
+					for(let j=0;j<3;j++) {
+						if(arr[j][i]==arr[j+1][i]) {
+							arr[j][i]+=arr[j+1][i];
+							for(let k=j+1;k<3;k++) {
+								arr[k][i]=arr[k+1][i];
+							}
+							arr[3][i]=0;
+						}
+					}
+				}
 			rannum();
 			display();break;
 		case "ArrowDown":
+				for(let i=0;i<4;i++) {
+					let count=3;
+					for(let j=3;j>=0;j--) {
+						if(arr[j][i]) {
+							arr[count--][i]=arr[j][i];
+						}
+					}
+					while(count>=0) {
+						arr[count--][i]=0;
+					}
+				}
+				for(let i=0;i<4;i++) {
+					for(let j=3;j>0;j--) {
+						if(arr[j][i]==arr[j-1][i]) {
+							arr[j][i]+=arr[j-1][i];
+							for(let k=j-1;k>0;k--) {
+								arr[k][i]=arr[k-1][i];
+							}
+							arr[0][i]=0;
+						}
+					}
+				}
 			rannum();
 			display();break;
 
